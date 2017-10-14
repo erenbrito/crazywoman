d5a750c1917a13024e61e7ecf92ed6e2c7f44859
Mariana Brito <mariana.bsouza@hotmail.com>
2017-10-14 18:26:47 -0300

    added UA code on config for google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,7 +122,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: ''
+        trackingId: 'UA-108058445-1'
       }
     },
     {
